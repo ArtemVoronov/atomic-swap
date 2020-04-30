32fcb5f48ce3f1b3a77855c03c79a45327233239
artem.voronov <artem.voronov@exyte.com>
2020-04-30 19:25:36 +0700

    added recreating of wallet from mnemonics

--- src/index.js
+++ src/index.js
@@ -1,21 +1,39 @@
-const {WalletClient, Network} = require('bcoin');
-const network = Network.get('testnet');
+const bcoin = require('bcoin').set('testnet');
 const apiKey = process.env.BCOIN_API_KEY;
+const network = bcoin.Network.get('testnet');
+const Mnemonic = bcoin.hd.Mnemonic;
+const HD = bcoin.hd;
 
+const clientOptions = {
+  network: network.type,
+  port: network.rpcPort,
+  apiKey: apiKey
+}
 const walletOptions = {
   network: network.type,
   port: network.walletPort,
   apiKey: apiKey
 }
 
-const walletClient = new WalletClient(walletOptions);
+const nodeClient = new bcoin.NodeClient(clientOptions);
+const walletClient = new bcoin.WalletClient(walletOptions);
 const wallet = walletClient.wallet("primary");
 
+async function createWalletFromMnemonics(mnemonicStr) {
+  const wdb = new bcoin.WalletDB({ db: 'memory' });
+  await wdb.open();
+  const mnemonic = new Mnemonic(mnemonicStr);
+  const masterKey = HD.fromMnemonic(mnemonic);
+  return await wdb.create({master: masterKey});
+}
 
 (async () => {
   const result = await wallet.getMaster();
+  console.log("original master:");
   console.log(result);
 
   const address = await wallet.createAddress("default");
+  console.log("original address:");
   console.log(address);
+
 })();
